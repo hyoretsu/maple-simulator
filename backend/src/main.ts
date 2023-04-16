c998b8b9bd74e8b992bee14507e4b7645c37b332
Aran Leite <hyoretsu@gmail.com>
2023-04-16 13:49:50 -0300

    Backend: fix Railway deploy
    NPM commands should now work

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -38,9 +38,9 @@ const bootstrap = async () => {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("docs", app, document);
 
-	const port = process.env.APP_PORT || 3333;
+	const port = process.env.PORT || 3333;
 
-	await app.listen(port);
+	await app.listen(port, "0.0.0.0");
 
 	console.log(`Server started on port ${port}!`);
 };
