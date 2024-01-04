656fc86c28c89daaf74405a899d0fe4ce5b8686f
Aran Leite <hyoretsu@gmail.com>
2024-01-04 17:33:32 -0300

    Fix Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,11 +67,11 @@ export default function RootLayout({ children }: PropsWithChildren) {
 				<Analytics />
 				<SpeedInsights />
 				<Script id="google-analytics">
-					{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)};gtag("js",new Date());gtag("config",${process.env.GA_MEASUREMENT_ID},{page_path: window.location.pathname})`}
+					{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)};gtag("js",new Date());gtag("config",${process.env.NEXT_PUBLIC_GA_ID},{page_path: window.location.pathname})`}
 				</Script>
 				<Script
 					strategy="afterInteractive"
-					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
 				/>
 				<WebVitals />
 			</body>
