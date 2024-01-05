004c02ad7cba3d9a2d89f9d899206a9ede75f84b
Aran Leite <hyoretsu@gmail.com>
2024-01-05 19:31:53 -0300

    Re-add Google Analytics

--- src/app/components/WebVitals.tsx
+++ src/app/components/WebVitals.tsx
@@ -22,4 +22,6 @@ export default function WebVitals() {
 			},
 		});
 	});
+
+	return <></>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,11 +69,15 @@ export default function RootLayout({ children }: PropsWithChildren) {
 				/>
 			</head>
 			<body>
+				<script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
+				<script id="google-analytics">
+					{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)};gtag("js",new Date());gtag("config","${process.env.NEXT_PUBLIC_GA_ID}",{page_path: window.location.pathname})`}
+				</script>
+
 				<Header />
 
 				<AccountProvider>{children}</AccountProvider>
 
-				{/* @ts-ignore */}
 				<WebVitals />
 			</body>
 		</html>
